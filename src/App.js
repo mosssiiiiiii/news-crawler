893a08cc9c88b8310f427ec6277ff29476c11eca
mosssiiiiiii <haghighatkhah.mohsen@yahoo.com>
2021-02-05 19:32:04 +0330

    Start develop header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from "./component/Header/Header";
 
 function App(){
     return(
         <div>
+            <Header/>
             hello world!
         </div>
     )
---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className='header-wrap'>
+
+        </div>
+    )
+}
+
+export default Header;
